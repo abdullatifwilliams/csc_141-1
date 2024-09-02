24269d56d077c19ce13b18f51ac49e099bc62213
abdullatifwilliams <williamsabdul27@gmail.com>
2024-09-02 09:15:38 -0400

    Delete hello_word.py

--- hello_word.py
+++
@@ -1,5 +0,0 @@
-print ("Hello world")
-print ("hello python crash course reader!")
-git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
-print (hello world)
